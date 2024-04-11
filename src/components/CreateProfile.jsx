f39e75badb153ee0faec7a04a3066b6d33c90ff0
surajgsn07 <surajgn07@gmail.com>
2024-04-11 10:13:27 +0530

    /

--- src/components/CreateProfile.jsx
+++ src/components/CreateProfile.jsx
@@ -29,7 +29,7 @@ const CreateProfile = () => {
 
 
     const response = await axios.post(
-      "https://aenaxy-backend.onrender.com/api/v1/user/setAvatar",
+      "https://aenaxy-backend.vercel.app/api/v1/user/setAvatar",
       data,
       {
         withCredentials: true,
@@ -71,7 +71,7 @@ const CreateProfile = () => {
 
       
       const response = await axios.post(
-        "hhttps://aenaxy-backend.onrender.com/api/v1/user/setLocation",
+        "https://aenaxy-backend.vercel.app/api/v1/user/setLocation",
         data,
         {
           withCredentials: true,
